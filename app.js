47b4f0aac7e5cfdcc0a2693ea163cb936c6c138a
rzvishnu <rzvishnu6@gmail.com>
2017-08-20 13:21:15 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-var express = require('express')
-var app = express()
-var port = process.env.PORT || 5678;
-app.use(express.static('public'));
-app.get('/', function (req, res) {
-  res.sendfile(__dirname+'/public/index.html');
-})
-app.listen(port)
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.render('index.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
